4ce9deeb5d1745512a7934e93a2342b0265de517
garret.meier <garret.meier@bunchball.com>
2016-07-07 23:36:36 -0700

    added headers

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(cors(corsOptions));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 
