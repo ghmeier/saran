b30bec405c2fd68fad335378e96b069037512d72
garret.meier <garret.meier@bunchball.com>
2016-07-07 23:30:18 -0700

    updating cors

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ var corsOptions = {
 
 app.use( bodyParser.json() );
 app.use(cors(corsOptions));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.set('port', (process.env.PORT || 5000));
 
