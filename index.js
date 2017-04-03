b0932cec4150782601e80661eee81c73f1943994
garret.meier <garret.meier@bunchball.com>
2017-04-03 17:30:45 -0500

    Add import endpoint

--- index.js
+++ index.js
@@ -7,44 +7,44 @@ const userHelper = require('./lib/user-helper.js');
 
 const app = express();
 const corsOptions = {
-	origin: '*'
+    origin: '*'
 };
 
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	res.header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-	next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    next();
 });
 
 app.set('view engine', 'ejs');
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', (request, response) => {
-	response.render('index', {
-		APP_ID: process.env.APP_ID,
-		SECRET: process.env.SECRET
-	});
+    response.render('index', {
+        APP_ID: process.env.APP_ID,
+        SECRET: process.env.SECRET
+    });
 });
 
 app.get('/signup', (request, response) => {
-	response.render('signup', {
-		APP_ID: process.env.APP_ID,
-		REDIRECT: process.env.REDIRECT
-	});
+    response.render('signup', {
+        APP_ID: process.env.APP_ID,
+        REDIRECT: process.env.REDIRECT
+    });
 });
 
 app.get('/callback', (request, response) => {
-	response.render('callback', {
-		APP_ID: process.env.APP_ID,
-		REDIRECT: process.env.REDIRECT
-	});
+    response.render('callback', {
+        APP_ID: process.env.APP_ID,
+        REDIRECT: process.env.REDIRECT
+    });
 });
 
 app.get('/js/functions.js', (request, response) => {
-	response.sendFile(path.join(__dirname, '/js/functions.js'));
+    response.sendFile(path.join(__dirname, '/js/functions.js'));
 });
 
 app.get('/token', tokenHelper.tokenList);
@@ -54,7 +54,8 @@ app.post('/token', tokenHelper.addToken);
 app.post('/user/:id/in', userHelper.checkIn);
 app.post('/user', userHelper.signup);
 app.get('/user', userHelper.userList);
+app.post('/import', userHelper.importUsers);
 
 app.listen(app.get('port'), () => {
-	console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
